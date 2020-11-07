66f910f9df9820c519afe9c40cb48df1409c2908
RajVadeghar <instigator0002@gmail.com>
2020-11-07 17:43:36 +0530

    Added comments and made some changes to Day 1 code

--- Day-1/revArray.cpp
+++ Day-1/revArray.cpp
@@ -1,3 +1,5 @@
+// C++ program to reverse an array 
+
 #include <iostream>
 using namespace std;
 
@@ -6,30 +8,47 @@ void reverse(int *array, int start, int end);
 void swap(int *a, int *b);
 
 int main() {
-    int array[] = {1, 4, 8, 7, 6, 9};
-    int size = sizeof(array) / sizeof(array[0]);
+    //specifying array size
+    int size;
+    cout << "Enter size of the array: \n";
+    cin >> size;
+
+    //specifying array
+    int array[size]; 
+    for (int i = 0; i < size; i++) {
+        cout << "Array Element " << i + 1 << ": ";
+        cin >> array[i];
+    }
+
     cout << "Given array is: \n";
     printArray(array, size);
+
     reverse(array, 0, size - 1);
     cout << "Output array is: \n";
     printArray(array, size);
 }
 
 void reverse(int *array, int start, int end) {
+    //if first value is greater than last exit out of the function
     if (start >= end) {
-        return;
+        return; 
     }
-    swap(&array[start], &array[end]);
-    reverse(array, start + 1, end - 1);
+    //swap first and last values
+    swap(&array[start], &array[end]); 
+
+    //change first and last values and then run the function again
+    reverse(array, start + 1, end - 1); 
 }
 
 void swap(int *a, int *b) {
+    //swapping a,b values
     *a = *a + *b;
     *b = *a - *b;
     *a = *a - *b;
 }
 
 void printArray(int *array, int size) {
+    //printing an array
     for (int i = 0; i < size; i++) {
         cout << array[i];
     }
