0fcf9c3f358108baf269ac3077ec469f30c50168
RajVadeghar <instigator0002@gmail.com>
2020-11-12 19:18:13 +0530

    Added comments to day 6 code -union of two arrays

--- Day-6/union.cpp
+++ Day-6/union.cpp
@@ -1,26 +1,33 @@
+// C++ program to find union count of two arrays 
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
+// functions used in this code
 void readArray(int *arrayA, int m);
 void printUnion(int *arr1, int *arr2, int m, int n);
 
 int main(void) {
+    //specifying number of test cases
     int testcases;
     cout << "Enter number of testcases: ";
     cin >> testcases;
+
     for (int i = 0; i < testcases; i++)
     {
+        //Specifying size of two arrays
         int m, n;
         cout << "\nEnter size of Array A and Array B: ";
         cin >> m >> n;
 
+        //specifying array
         int arrayA[m], arrayB[n];
         cout << "Enter elements of array A";
         readArray(arrayA, m);
         cout << "Enter elements of array B";
         readArray(arrayB, n); 
 
+        //sorting arrays
         sort(arrayA, arrayA + m);
         sort(arrayB, arrayB + n);
 
@@ -29,6 +36,9 @@ int main(void) {
 }
 
 void printUnion(int *arr1, int *arr2, int m, int n) {
+    // i is the initial index of arr1
+    // j is the initial index of arr2
+    // count to to keep track of every unique element
     int i = 0, j = 0, count = 0; 
     while (i < m && j < n) { 
         if (arr1[i] < arr2[j]) {
