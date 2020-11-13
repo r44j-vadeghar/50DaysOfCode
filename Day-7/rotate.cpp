7033015b382ad644085c12a73ce7191c04294fb6
RajVadeghar <instigator0002@gmail.com>
2020-11-13 18:33:03 +0530

    Added comments to day 7 code -rotate

--- Day-7/rotate.cpp
+++ Day-7/rotate.cpp
@@ -1,20 +1,25 @@
+// C++ program to rotate an array by one
 #include <iostream>
 using namespace std;
 
+// functions used in this code
 void rotateArr(int *array, int size);
 void printArray(int *array, int size);
 
 int main(void) {
+    //specifying number of test cases
     int testcases;
     cout << "Enter number of testcases: ";
     cin >> testcases;
 
     for (int i = 0; i < testcases; i++)
     {
+        //Specifying size of two arrays
         int size;
         cout << "\nEnter size of Array: ";
         cin >> size;
 
+        //specifying array
         cout << "\nEnter elements of Array: ";
         int array[size];
         for (int i = 0; i < size; i++) {
@@ -27,11 +32,16 @@ int main(void) {
 }
 
 void rotateArr(int *array, int size) {
+    // duplicating last element
     int temp = array[size - 1];
+
+    // rotating all elements by 1(except 0th location)
     for (int i = size - 1; i > 0; i--)
     {
         array[i] = array[i - 1];
     }
+
+    // putting duplicated element into 0th laction of array
     array[0] = temp;
 }
 
