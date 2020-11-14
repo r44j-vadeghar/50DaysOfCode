36dd3676643ee4b692af7140e21098cbbf2d1a4d
RajVadeghar <instigator0002@gmail.com>
2020-11-14 20:17:06 +0530

    Added day-8 code -difference of max and min heights

---
+++ Day-8/diff.cpp
@@ -0,0 +1,47 @@
+// C++ program to rotate an array by one
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// functions used in this code
+int getMinDiff(int *array, int size, int k);
+
+int main(void) {
+    //specifying number of test cases
+    int testcases;
+    cout << "Enter number of testcases: ";
+    cin >> testcases;
+
+    for (int i = 0; i < testcases; i++)
+    {
+        //Specifying size of two arrays
+        int size, k;
+        cout << "\nEnter size of Array: ";
+        cin >> size;
+
+        cout << "\nEnter k value: ";
+        cin >> k;
+
+        //specifying array
+        cout << "\nEnter elements of Array: ";
+        int array[size];
+        for (int i = 0; i < size; i++) {
+            cin >> array[i];
+        }
+        int output = getMinDiff(array, size, k);
+        cout << output;
+    }
+}
+
+int getMinDiff(int *array, int size, int k) {
+    for (int i = 0; i < size; i++) {
+        if (array[i] - k <= 0) {
+            array[i] = array[i] + k;
+        }
+        else {
+            array[i] = array[i] - k;
+        }
+    }
+    sort(array, array + size);
+    return array[size - 1] - array[0];
+}
