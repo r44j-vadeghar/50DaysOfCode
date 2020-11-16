f07c20544dce05e4d3648a9f7d4643df0109e456
RajVadeghar <instigator0002@gmail.com>
2020-11-16 18:21:50 +0530

    Added comments to day 10 code

--- Day-10/duplicate.cpp
+++ Day-10/duplicate.cpp
@@ -1,20 +1,26 @@
+// C++ program to find duplicates in an array
 #include <iostream>
 #include <vector>
 #include<cmath>
 using namespace std;
 
+// functions used in this code
 void findDuplicate(vector<int> &nums);
 
 int main(void) {
+    // vector array
     vector<int> nums;
+
     int size, element;
+    // specifying size of array
     cout << "Enter number of elements in array: ";
     cin >> size;
 
+    // pushing element to vector array
     for (int i = 0; i < size; i++) {
         cout << "\nEnter an element " << i << " to add to this vector";
         cin >> element;
-        nums.push_back(element);
+        nums.push_back(element); 
     }
     findDuplicate(nums);
 }
