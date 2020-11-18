31bfc765b485aa3386d247106f71773f739fea40
RajVadeghar <instigator0002@gmail.com>
2020-11-18 16:29:24 +0530

    Added comments to day 12 code

--- Day-12/kadanes.cpp
+++ Day-12/kadanes.cpp
@@ -1,19 +1,26 @@
+// C++ code to find largest sum contiguous sub array
 #include <iostream>
 #include<climits>
 using namespace std;
 
+// Functions used in this code
 int maxSubarray(int *array, int size);
 
+// Driver function
 int main(void) {
+    // Specifying number of test cases
     int testcases;
     cout << "Enter number of testcases: ";
     cin >> testcases;
+
     for (int i = 0; i < testcases; i++)
     {
+        // Specifying size of array
         int size;
         cout << "\nEnter the size of the array: ";
         cin >> size;
 
+        // Initialize the array elements
         int array[size];
         cout << "\nEnter the array elements: ";
         for (int i = 0; i < size; i++)
@@ -21,10 +28,12 @@ int main(void) {
             cin >> array[i];
         }
         
+        // print output onto the screen
         cout << maxSubarray(array, size);
     } 
 }
 
+// function to find largest sum contiguous sub array
 int maxSubarray(int *array, int size) {
     int max = INT_MIN, max_ending = 0;
     for (int i = 0; i < size; i++) {
