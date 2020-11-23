0cf1a00b1b36cbd92cebd3dfe7b7b01cfafe1549
rajvadeghar <rajvadeghar@kali.rajvadeghar.com>
2020-11-23 19:04:14 +0530

    Added comments and some stuff to getPairsCount code

--- Day-17/getPairsCount.cpp
+++ Day-17/getPairsCount.cpp
@@ -1,47 +1,44 @@
+// C++ program to find count pairs with given sum 
 #include <iostream>
 using namespace std;
 
+// class with public function
 class Solution {   
 public:
+
+    //function to get count of pairs with given sum
     int getPairsCount(int *array, int size, int k) {
         int count = 0;
-        /* for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
                 if (array[i] + array[j] == k) {
                     count++;
                 }
             }
-        }  */
-        for (int i = 0; i < size; i++)
-        {
-            switch (array[i])
-            {
-            case :
-                /* code */
-                break;
-            
-            default:
-                break;
-            }
         }
-        
         return count;
     }
 };
 
+// Driver Code
 int main(void) {
+    // Specifying size of array and k value
     int size, k;
     cout << "Enter size of the array: ";
     cin >> size;
     cout << "Enter k value: ";
     cin >> k;
 
+    // specifying array
     int array[size];
     cout << "Enter elements of array: ";
     for (int i = 0; i < size; i++)
     {
         cin >> array[i];
     }
+
+    // creating object obj for solution class
     Solution obj;
+    // displaying output to the usre
     cout << obj.getPairsCount(array, size, k) << endl;
 }
