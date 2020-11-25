ff2396c0d4ca5cdb71cd6584ec28e8d89b29cea0
RajVadeghar <instigator0002@gmail.com>
2020-11-25 18:22:20 +0530

    Added comments to day 19 code -smallestSubArrayWithGreatestSum

--- Day-19/smallestSubArrayWithGreatestSum.cpp
+++ Day-19/smallestSubArrayWithGreatestSum.cpp
@@ -1,33 +1,43 @@
+// C++ program to find smallest sub array count with greatest sum
 #include <iostream>
 #include <climits>
 using namespace std;
 
+// functions used in this code
 int smallestSubArrayWithGreatestSum(int *array, int size, int totalSum);
 int maximun(int *array, int size);
 
+//Driver code
 int main(void) {
+    // Specifying number of testcases
     int testcases;
     cout << "Enter number of testcases: ";
     cin >> testcases;
 
+    // traversing through each testcase
     for(int i = 0; i < testcases; i++) {
+        // Specifying size of the array
         int size, sum;
         cout << "Enter size of the array: ";
         cin >> size;
 
+        // Specifying the array and it's elements
         int array[size];
         cout << "Enter elements of array: ";
         for(int i = 0; i < size; i++) {
             cin >> array[i];
         }
 
+        // Specifying sum value
         cout << "Enter your sum: ";
         cin >> sum;
 
+        // Displaying output on the screen
         cout << smallestSubArrayWithGreatestSum(array, size, sum) << endl;
     }
 }
 
+// function to find smallest sub array count with greatest sum
 int smallestSubArrayWithGreatestSum(int *array, int size, int totalSum) {
     int counter = INT_MAX, error = 0;
     for(int i = 0; i < size; i++) {
